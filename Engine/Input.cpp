3f9c9e973114f905775f590cc9e3a6dfa9c9f819
Ed94 <edwardgz@gmail.com>
2021-02-11 02:01:03 -0500

    made some naming changes in input (not following convention...

--- Engine/Input.cpp
+++ Engine/Input.cpp
@@ -44,60 +44,42 @@ void Input::Update(void)
 {
 	for (uIntDM index = 0; index < Keys_NumTracked; index++)
 	{
-		bool Current, Previous;
+		bool current, previous;
 
 		// Get current signal state
 
 		Context.PreviousSignalState.Array[index] = Context.CurrentSignalState.Array[index];
 
-		Previous = Context.CurrentSignalState.Array[index];
+		previous = Context.CurrentSignalState.Array[index];
 
 		Context.CurrentSignalState.Array[index] = GetKeySignal(GetKeyCodeAtIndex(index));
 
-		Current = Context.CurrentSignalState.Array[index];
+		current = Context.CurrentSignalState.Array[index];
 
 		// Determine latest key state.
 
-		EState& CurrentState = Context.KeyStates[index];
+		EState& currentState = Context.KeyStates[index];
 				
 		EState latestState = EState::None;
 
-		if (Current == Previous)
+		if (current == previous)
 		{
-			if (Current == true)
-			{
-				latestState = EState::PressHeld;
-			}
-			else
-			{
-				if (CurrentState != EState::PressHeld)
-				{
-					latestState = EState::None;
-				}
-			}
+			if      (current      == true             ) latestState = EState::PressHeld;
+			else if (currentState != EState::PressHeld) latestState = EState::None;
 		}
 		else
 		{
-			if (Current == false)
-			{
-				latestState = EState::Released;
-			}
-			else
-			{
-				latestState = EState::Pressed;
-			}
+			if   (current == false) latestState = EState::Released;
+			else                    latestState = EState::Pressed;
 		}
 
-		if (latestState != CurrentState)
+		if (latestState != currentState)
 		{
-			CurrentState = latestState;
+			currentState = latestState;
 
 			for (auto& sub : Context.KeyEventSubs[index])
 			{
-				if (sub != nullptr)
-				{
-					dref(sub)(CurrentState);
-				}
+				if (sub != nullptr) dref(sub)(currentState);
 			}
 		}
 	}
@@ -106,6 +88,16 @@ void Input::Update(void)
 void Input::SubscribeTo(EKeyCode _key, EventFunction& _callbackFunction)
 {
 	Subscriptions& subs = Context.KeyEventSubs[GetKeyIndexFromCode(_key)];
+
+	for (auto& sub : subs)
+	{
+		if (sub == nullptr)
+		{
+			sub = getPtr(_callbackFunction);
+
+			return;
+		}
+	}
 
 	subs.push_back(getPtr(_callbackFunction));
 }
